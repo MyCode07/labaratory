28425063851b78ab1b8d330fedbf9f9600461d8e
myname <erem151999@mail.ru>
2022-03-05 00:42:28 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+// document.addEventListener('click', function (e) {
+//     let targetEl = e.target;
+//     if (targetEl.classList.contains('header__burger')) {
+//         targetEl.classList.toggle('_active');
+//         document.querySelector('.header__menu').classList.toggle('_active');
+//         document.querySelector('.page').classList.toggle('_moved');
+//     }
+// });
+
+// if (document.querySelector('.feedback__slider')) {
+//     new Swiper('.feedback__slider', {
+//         slidesPerView: 'auto',
+//         loop: true,
+//         spaceBetween: 19,
+//         grabCursor: true,
+//         pagination: {
+//             el: '.feedback__slider-pagination',
+//             clickable: true,
+//         },
+
+//     })
+// }
+
+
